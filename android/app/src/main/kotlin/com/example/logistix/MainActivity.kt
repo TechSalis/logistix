937eb5820d3bb5e52723482cda135e0e54ce4284
Eric Onyeulo <onyeuloeric@gmail.com>
2025-06-15 13:04:31 +0100

    Project setup

---
+++ android/app/src/main/kotlin/com/example/logistix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.logistix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:logistix/src/app.dart';
+// import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+void main() {
+  // MapboxOptions.setAccessToken(ACCESS_TOKEN);
+  runApp(const MainApp());
+}
