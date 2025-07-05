9d4b1642ae7b8f64297748ce5c4416348c881634
Eric Onyeulo <onyeuloeric@gmail.com>
2025-07-05 20:12:04 +0100

    feat: Added Image Picker package and Usecase class

---
+++ lib/core/usecases/pick_image.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class PickImageUsecase {
+  final ImagePicker _picker;
+
+  PickImageUsecase({ImagePicker? picker}) : _picker = picker ?? ImagePicker();
+
+  Future<File?> call() async {
+    final result = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+    );
+    if (result != null) return File(result.path);
+
+    return null;
+  }
+}
